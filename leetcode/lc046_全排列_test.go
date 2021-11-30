0f6f9068734625da7a25c451098a65729b0473f3
xvym <xdxvym@163.com>
2021-12-01 00:08:00 +0800

    update: lc 046

---
+++ leetcode/lc046_全排列_test.go
@@ -0,0 +1,40 @@
+/**
+ * @author: xv
+ * @date: 2021/11/30 23:41
+ */
+
+package leetcode
+
+import "testing"
+
+func TestLc046(t *testing.T) {
+
+}
+
+func permute(nums []int) [][]int {
+	res := make([][]int, 0)
+	path := make([]int, len(nums))
+	visit := make([]int, len(nums))
+	for i := range visit {
+		visit[i] = 0
+	}
+	res = backtrack046(0, res, path, nums, visit)
+	return res
+}
+
+func backtrack046(index int, res [][]int, path []int, nums []int, visit []int) [][]int {
+	if index == len(nums) {
+		temp := make([]int, index)
+		copy(temp, path)
+		res = append(res, temp)
+	}
+	for i := 0; i < len(nums); i++ {
+		if visit[i] != 1 {
+			visit[i] = 1
+			path[index] = nums[i]
+			backtrack046(index + 1, res, path, nums, visit)
+			visit[i] = 0
+		}
+	}
+	return res
+}
